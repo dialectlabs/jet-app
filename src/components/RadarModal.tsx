8ab0768c621fcafc6668fe7c63c066c20d43181b
Tristyn <tristynstimpson@gmail.com>
2022-05-09 16:00:46 -0700

    Update packages

--- src/components/RadarModal.tsx
+++ src/components/RadarModal.tsx
@@ -1,65 +1,59 @@
-import { useEffect, useState } from 'react';
 import { useRadarModal } from '../contexts/radarModal';
-import { useDarkTheme } from '../contexts/darkTheme';
 import { useLanguage } from '../contexts/localization/localization';
 import { useTradeContext } from '../contexts/tradeContext';
 import { Modal } from 'antd';
-import { RateObserver, ProtocolRates } from '@jet-lab/defi-yield-ts';
 import { ReactComponent as RadarIcon } from '../styles/icons/radar_icon.svg';
-import { idl, cluster } from '../hooks/jet-client/useClient';
 
 export function RadarModal(): JSX.Element {
-  const { darkTheme } = useDarkTheme();
+  // const { darkTheme } = useDarkTheme();
   const { dictionary } = useLanguage();
   const { currentReserve } = useTradeContext();
   const { radarOpen, setRadarOpen } = useRadarModal();
-  const [protocols, setProtocols] = useState([
-    {
-      name: 'jet',
-      rates: {} as any
-    },
-    {
-      name: 'mango',
-      rates: {} as any
-    },
-    {
-      name: 'apricot',
-      rates: {} as any
-    },
-    {
-      name: 'port',
-      rates: {} as any
-    },
-    {
-      name: 'solend',
-      rates: {} as any
-    }
-  ]);
+  // const protocols = [
+  //   {
+  //     name: 'jet',
+  //     rates: {} as any
+  //   },
+  //   {
+  //     name: 'mango',
+  //     rates: {} as any
+  //   },
+  //   {
+  //     name: 'apricot',
+  //     rates: {} as any
+  //   },
+  //   {
+  //     name: 'port',
+  //     rates: {} as any
+  //   },
+  //   {
+  //     name: 'solend',
+  //     rates: {} as any
+  //   }
+  // ];
 
   // Fetch/update rates on init and every 15 seconds
-  useEffect(() => {
-    async function getRates() {
-      const rateObserver = new RateObserver();
-      const rates: ProtocolRates[] = await rateObserver.fetchAll(idl.metadata.cluster);
-      for (const rate of rates) {
-        for (const protocol of protocols) {
-          if (rate.protocol === protocol.name) {
-            for (const r of rate.rates) {
-              protocol.rates[r.asset] = {
-                deposit: r.depositRate,
-                borrow: r.borrowRate
-              };
-            }
-          }
-        }
-      }
+  // useEffect(() => {
+  //   async function getRates() {
+  //     const rateObserver = new RateObserver();
+  //     const rates: ProtocolRates[] = await rateObserver.fetchAll(idl.metadata.cluster);
+  //     for (const rate of rates) {
+  //       for (const protocol of protocols) {
+  //         if (rate.protocol === protocol.name) {
+  //           for (const r of rate.rates) {
+  //             protocol.rates[r.asset] = {
+  //               deposit: r.depositRate,
+  //               borrow: r.borrowRate
+  //             };
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
 
-      setProtocols([...protocols]);
-    }
-
-    getRates();
-    setInterval(() => getRates(), 15000);
-  }, []);
+  //   getRates();
+  //   setInterval(() => getRates(), 15000);
+  // }, []);
 
   return (
     <Modal footer={null} visible={radarOpen} className="radar-modal" onCancel={() => setRadarOpen(false)}>
@@ -87,7 +81,8 @@ export function RadarModal(): JSX.Element {
             </tr>
           </thead>
           <tbody>
-            {protocols.map((protocol, i) => (
+            <h2>Radar is temporarily unavailable</h2>
+            {/* {protocols.map((protocol, i) => (
               <tr
                 key={protocol.name}
                 className={`no-interaction ${(i + 1) % 2 === 0 ? 'row-bg' : ''} ${darkTheme ? '' : 'white'}`}>
@@ -108,7 +103,7 @@ export function RadarModal(): JSX.Element {
                     : '--'}
                 </td>
               </tr>
-            ))}
+            ))} */}
           </tbody>
         </table>
       </div>
